8eeb9009ffe85c3825c12ad650dd255321e4cbea
Nakahashi2016 <167910702+Nakahashi2016@users.noreply.github.com>
2025-07-14 17:18:11 -0300

    Add files via upload

---
+++ aulaJava1/src/aula00/aula02.java
@@ -0,0 +1,19 @@
+package aula00;
+
+    public class aula02 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+        System.out.println("10 > 2 " + (a > b));
+        System.out.println("10 < 2 " + (a < b));
+        System.out.println("2 = 2 " + (b == b));
+        System.out.println("10 = 2 " + (a == b));
+        System.out.println("10 / 2 = " + (a / b));
+        System.out.println("10 * 2 = " + (a * b));
+        System.out.println("2 % 10 = " + (b % a));
+
+
+
+    }
+}
